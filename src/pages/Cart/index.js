3550063b939b1c2975ceaa3b9ec3fc52a2596320
Gabriel Uchoa de Vasconcelos <gabriel.uchoa.vasconcelos@gmail.com>
2020-11-28 17:48:44 -0300

    feat: update cart page to show products add in cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,67 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
 
 function Cart() {
-  return <div />;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th> PRODUTO </th>
+            <th> QTD </th>
+            <th> SUBTOTAL </th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_zoom2.jpg?ts=1571078789&ims=326x"
+                alt="sapatos"
+              />
+            </td>
+            <td>
+              <strong>Tênis muito caro</strong>
+              <span>R$ 990,90</span>
+            </td>
+            <td>
+              <div>
+                <button>
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button>
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 1981,8</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button"> Finalizar pedido </button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>1920,2</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
 
 export default Cart;
