ca4fb3c20ae9f3c672c3282fe5ec350957c03dba
Gabriel Uchoa de Vasconcelos <gabriel.uchoa.vasconcelos@gmail.com>
2020-12-02 23:43:39 -0300

    feat: connect home to redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,8 @@ import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 
+import { connect } from 'react-redux';
+
 import { ProductList } from './styles';
 
 class Home extends Component {
@@ -21,6 +23,15 @@ class Home extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = (product) => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
+
   render() {
     const { products } = this.state;
 
@@ -31,7 +42,10 @@ class Home extends Component {
             <img src={product.image} alt={product.title} />
             <strong> {product.title} </strong>
             <span>{product.priceFormatted} </span>
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" /> 3
               </div>
@@ -44,4 +58,4 @@ class Home extends Component {
   }
 }
 
-export default Home;
+export default connect()(Home);
