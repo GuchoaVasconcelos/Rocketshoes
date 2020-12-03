936f1176a83140d3da6a88f05d9cf0e308fbee54
Gabriel Uchoa de Vasconcelos <gabriel.uchoa.vasconcelos@gmail.com>
2020-12-02 23:44:04 -0300

    refactor: routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
+import Home from './pages/Home/index';
 import Cart from './pages/Cart';
 
 export default function Routes() {
