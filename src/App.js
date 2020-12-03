9c07fb6888c7d6c8c8d5b36668b20d1780050702
Gabriel Uchoa de Vasconcelos <gabriel.uchoa.vasconcelos@gmail.com>
2020-12-03 11:03:13 -0300

    feat: import config of reactotron in the app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
+import './config/ReactotronConfig';
 import store from './store/store';
 import { Provider } from 'react-redux';
 
