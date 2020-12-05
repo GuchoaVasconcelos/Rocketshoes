971c37d8f8143b4cb54e27675e91bd6608a59f0a
Gabriel Uchoa de Vasconcelos <gabriel.uchoa.vasconcelos@gmail.com>
2020-12-05 16:36:57 -0300

    feat: change BrowserRouter to Router and add history as props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
 import Routes from './routes';
+import history from './services/history';
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
 import { ToastContainer } from 'react-toastify';
@@ -11,12 +12,12 @@ import { Provider } from 'react-redux';
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyle />
         <ToastContainer autoClose={3000} />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
